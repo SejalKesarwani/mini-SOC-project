107d88adfdc5f8acddc173519ba24b87b3338361
SejalKesarwani <sejal.2025ca087@mnnit.ac.in>
2026-01-15 23:30:30 +0530

    Initialize Express server with basic endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
+// backend/server.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Mini SOC backend is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
 
